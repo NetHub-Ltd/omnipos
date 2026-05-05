c44a6daabde9aa7f95e744e5ce024963350be2e0
Davie Karanja <80883320+daviekaranja@users.noreply.github.com>
2026-05-05 20:23:02 +0300

    this commit adds entrypoint.sh, this is responsible for running migrations and starting the wep app

---
+++ backend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Running database migrations..."
+alembic upgrade head
+
+echo "Starting Gunicorn..."
+# main:app is correct because main.py is in the WORKDIR (/app)
+# app.lib imports will work because the 'app' folder is also in /app
+exec gunicorn -w 2 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8000 --timeout 120
